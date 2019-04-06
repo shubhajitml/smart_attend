ef4b5733c4e3a8e1c28036c735be687e950fcbb8
Shubhajitdas121 <shubhajitdas121@gmail.com>
2019-04-06 19:12:24 +0530

    updated db

--- biometric.sql
+++ biometric.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 05, 2019 at 01:13 PM
+-- Generation Time: Apr 06, 2019 at 07:11 PM
 -- Server version: 5.7.25-0ubuntu0.18.04.2
 -- PHP Version: 7.2.15-0ubuntu0.18.04.2
 
@@ -64,12 +64,15 @@ CREATE TABLE `attendance_sheet` (
 --
 
 INSERT INTO `attendance_sheet` (`attandance_id`, `fk_subject_id`, `date`, `time`, `fk_student_id`) VALUES
-(1, 2, '2019-03-06', '19:43:53', 1),
-(2, 2, '2019-03-06', '23:23:38', 2),
-(3, 1, '2019-03-06', '19:43:53', 1),
-(4, 2, '2019-04-04', '13:24:30', 3),
-(7, 3, '2019-04-04', '13:25:44', 3),
-(8, 1, '2019-04-04', '13:25:50', 3);
+(1, 2, '2019-04-06', '17:46:41', 3),
+(2, 2, '2019-04-06', '17:46:46', 1),
+(3, 2, '2019-04-06', '17:46:49', 2),
+(4, 2, '2019-04-06', '17:46:53', 6),
+(5, 2, '2019-04-06', '17:47:15', 5),
+(6, 2, '2019-04-06', '17:47:19', 4),
+(7, 1, '2019-04-06', '17:47:42', 3),
+(8, 1, '2019-04-06', '17:48:40', 5),
+(9, 1, '2019-04-06', '17:48:44', 4);
 
 -- --------------------------------------------------------
 
@@ -79,7 +82,7 @@ INSERT INTO `attendance_sheet` (`attandance_id`, `fk_subject_id`, `date`, `time`
 
 CREATE TABLE `faculty_users` (
   `fac_id` int(11) NOT NULL,
-  `fac_reg_no` varchar(100) NOT NULL,
+  `fac_reg_no` bigint(100) NOT NULL,
   `fac_fing_serial` int(10) NOT NULL,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
@@ -94,8 +97,8 @@ CREATE TABLE `faculty_users` (
 --
 
 INSERT INTO `faculty_users` (`fac_id`, `fac_reg_no`, `fac_fing_serial`, `name`, `email`, `mobile`, `branch`, `created_by`, `created_on`) VALUES
-(1, 'CSE002', 0, 'Omkar Pattanaik', 'omkarpattanaik@gcekjr.ac.in', 9439413440, 'Computer Science and Engineering', 1, '2019-03-06 19:08:08'),
-(2, 'CSE003', 1, 'Sanjit Kumar Barik', 'sanjitbarik@gcekjr.ac.in', 9937583247, 'Computer Science and Engineering', 1, '2019-03-06 19:09:44');
+(2, 3, 1, 'Sanjit Kumar Barik', 'sanjitbarik@gcekjr.ac.in', 9937583247, 'Computer Science and Engineering', 1, '2019-03-06 19:09:44'),
+(3, 2, 170, 'Omkar Pattanaik', 'omkarpattanaik@gcekjr.ac.in', 9439413440, 'Computer Science and Engineering', 1, '2019-03-06 19:08:08');
 
 -- --------------------------------------------------------
 
@@ -110,6 +113,14 @@ CREATE TABLE `fac_attendance_sheet` (
   `time` time NOT NULL,
   `fk_faculty_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `fac_attendance_sheet`
+--
+
+INSERT INTO `fac_attendance_sheet` (`attandance_id`, `fk_subject_id`, `date`, `time`, `fk_faculty_id`) VALUES
+(1, 2, '2019-04-06', '17:45:33', 3),
+(2, 1, '2019-04-06', '17:47:33', 2);
 
 -- --------------------------------------------------------
 
@@ -137,7 +148,7 @@ INSERT INTO `students` (`stu_id`, `reg_no`, `stu_fing_serial`, `name`, `branch`,
 (1, 1501104078, 151, 'Sasmita Dash', 'Computer Science and Engineering', 'sasmita1798@gmail.com', 8763656601, 1, '2019-03-06 19:11:01'),
 (2, 1501104083, 152, 'Sonali Priyadarsini Rout', 'Computer Science and Engineering', 'a.s.sonali.rout@gmail.com', 9348216431, 1, '2019-03-06 19:13:55'),
 (3, 1501104080, 150, 'Shubhajit Das', 'Computer Science and Engineering', 'shubhajitdas121@gmail.com', 8280144660, 1, '2019-03-06 21:53:54'),
-(4, 1501104054, 161, 'Amitabh Sekhar Mishra', 'Computer Science and Engineering', 'amitabhmishra592@gmail.com', 7064207517, 155, '2019-03-09 23:04:16'),
+(4, 1501104054, 155, 'Amitabh Sekhar Mishra', 'Computer Science and Engineering', 'amitabhmishra592@gmail.com', 7064207517, 155, '2019-03-09 23:04:16'),
 (5, 1501104058, 154, 'Ashirbad Samantaray', 'Computer Science and Engineering', 'contact@ashirbad.me', 7008917985, 1, '2019-03-09 23:08:08'),
 (6, 1501104055, 153, 'Ananya Jena', 'Computer Science and Engineering', 'ajena635@gmail.com', 7978079693, 1, '2019-03-09 23:12:12');
 
@@ -185,16 +196,16 @@ ALTER TABLE `attendance_sheet`
 -- Indexes for table `faculty_users`
 --
 ALTER TABLE `faculty_users`
-  ADD PRIMARY KEY (`fac_id`);
+  ADD PRIMARY KEY (`fac_id`),
+  ADD UNIQUE KEY `fac_reg_no` (`fac_reg_no`),
+  ADD UNIQUE KEY `fac_fing_serial` (`fac_fing_serial`);
 
 --
 -- Indexes for table `fac_attendance_sheet`
 --
 ALTER TABLE `fac_attendance_sheet`
   ADD PRIMARY KEY (`attandance_id`),
-  ADD UNIQUE KEY `fk_subject_id` (`fk_subject_id`),
-  ADD UNIQUE KEY `date` (`date`),
-  ADD UNIQUE KEY `fk_faculty_id` (`fk_faculty_id`);
+  ADD UNIQUE KEY `fac_attend_unique` (`fk_subject_id`,`date`,`fk_faculty_id`);
 
 --
 -- Indexes for table `students`
@@ -225,7 +236,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `attendance_sheet`
 --
 ALTER TABLE `attendance_sheet`
-  MODIFY `attandance_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `attandance_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `faculty_users`
 --
@@ -235,7 +246,7 @@ ALTER TABLE `faculty_users`
 -- AUTO_INCREMENT for table `fac_attendance_sheet`
 --
 ALTER TABLE `fac_attendance_sheet`
-  MODIFY `attandance_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `attandance_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `students`
 --
